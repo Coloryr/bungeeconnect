38391d5820e6f43b82ce47300325461b45982113
Color_yr <402067010@qq.com>
2019-03-17 01:55:26 +0800

    Initial commit

---
+++ src/main/java/Color_yr/BungeeConnect/BungeeConnect.java
@@ -0,0 +1,100 @@
+package Color_yr.BungeeConnect;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.logging.Logger;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+
+public class BungeeConnect extends Plugin
+{
+	public static String Version = "1.0.0";
+
+	public static String Server1710;
+	public static String Server1122;
+	public static String Server1132;
+
+	public static String Servers;
+
+	public static Configuration config;
+	private static File FileName;
+	
+	public static Logger log = ProxyServer.getInstance().getLogger();
+
+	public static void loadconfig() 
+	{
+		log.info("[BungeeConnect]你的配置文件版本是：" + config.getString("Version"));
+
+		Server1710 = config.getString("Server1710", "heartage0");
+		Server1122 = config.getString("Server1122", "heartage0");
+		Server1132 = config.getString("Server1132", "heartage0");
+
+		Servers = config.getString("Servers", "heartage0");
+	}
+
+	public static void reloadConfig() 
+	{
+		try 
+		{
+			config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(FileName);
+			loadconfig();
+		}
+		catch (Exception arg0) 
+		{
+			log.warning("[BungeeConnect]配置文件读取错误：" + arg0.getMessage());
+		}
+	}
+
+	public static Configuration getConfig() 
+	{
+		return config;
+	}
+
+	public void setConfig() 
+	{
+		FileName = new File(getDataFolder(), "config.yml");
+		logs.file = new File(getDataFolder(), "logs.log");
+		if (!getDataFolder().exists())
+			getDataFolder().mkdir();
+		if (!FileName.exists()) 
+		{
+			try (InputStream in = getResourceAsStream("config.yml"))
+			{
+				Files.copy(in, FileName.toPath());
+			} 
+			catch (IOException e) 
+			{
+				log.warning("[BungeeConnect]配置文件创建错误：" + e.getMessage());
+			}
+		}
+		try {
+			if (!logs.file.exists()) {
+				logs.file.createNewFile();
+			}
+		} catch (IOException e) {
+			log.warning("§d[BungeeConnect]§c日志文件错误：" + e);
+		}
+	}
+	
+	@Override
+	public void onEnable() 
+	{		
+		log.info("[BungeeConnect]正在启动，插件交流群：571239090欢迎加入");
+		setConfig();
+		reloadConfig();
+		ProxyServer.getInstance().getPluginManager().registerListener(this,new Event());
+		log.info("[BungeeConnect]已启动，你运行的版本是：" + Version);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onDisable() 
+	{
+		log.info("[BungeeConnect]已停止运行，欢迎再次使用");
+	}
+}
---
+++ src/main/java/Color_yr/BungeeConnect/logs.java
@@ -0,0 +1,32 @@
+package Color_yr.BungeeConnect;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+public class logs {
+	public static File file;
+
+	public void log_write(String text) {
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter(file, true);
+			Date date = new Date();
+			String year = String.format("%tF", date);
+			String time = String.format("%tT", date);
+			String write = "[" + year + "]" + "[" + time + "]" + text;
+			PrintWriter pw = new PrintWriter(fw);
+			pw.println(write);
+			pw.flush();
+			fw.flush();
+			pw.close();
+			fw.close();
+		} catch (FileNotFoundException e) {
+			BungeeConnect.log.warning("§d[BungeeConnect]§c日志文件写入失败" + e);
+		} catch (IOException e) {
+			BungeeConnect.log.warning("§d[BungeeConnect]§c日志文件写入失败" + e);
+		}
+	}
+}
